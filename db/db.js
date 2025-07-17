30871a566d6fb18980eba238c9d4057df69da0fb
priyansu57 <priyansupriyajyotij@gmail.com>
2025-07-17 14:03:53 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
  async function connectDB() {
-    await mongoose.connect(process.env.DB)
+    await mongoose.connect(process.env.MONGO_URI)
     
  }
 connectDB()
